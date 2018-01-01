d7bfa239c7b07df49ee07b5a58b4321d4ec6bdf5
Rempart <pierre.martin@live.com>
2018-01-01 01:58:08 +0100

    Complete experiences

--- src/components/work/RiWork.vue
+++ src/components/work/RiWork.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
 .ri-work
   h1(v-t="'title'")
+  p(v-t="'summary'")
   ul.ri-block__experiences
     li.ri-block__experience(v-for="experience in experiences", :key="experience.id")
       ri-block(:experience="experience")
@@ -35,8 +36,14 @@ export default {
 
 <i18n>
   {
-    "en": { "title": "Work" },
-    "fr": { "title": "Travail" }
+    "en": {
+      "title": "Short long stories",
+      "summary": "Here are all my experiences. Some are success, others are failures but I always did my best."
+    },
+    "fr": {
+      "title": "Quelques histoires courtes",
+      "summary": "Voici mes différentes expériences. Certaines se sont bien terminées, d'autres beaucoup moins bien ; quoiqu'il en soit, j'ai toujorus fait de mon mieux."
+    }
   }
 </i18n>
 
