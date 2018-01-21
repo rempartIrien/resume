2a744833cb64079746d285d4632705320cf765ea
Rempart <pierre.martin@live.com>
2018-01-22 00:39:57 +0100

    Add 2018 dates for dot conferences

--- src/components/profile/RiConferences.vue
+++ src/components/profile/RiConferences.vue
@@ -4,11 +4,15 @@
       li.ri-conferences__item
         a(href="http://dotjs.io")
           img.ri-conferences__logo(src="../../assets/logos/logo-dotjs.png", alt="dotJS logo")
-        p.ri-conferences__caption(v-t="'dotjs'")
+        ul.ri-conferences__caption
+          li(v-t="'dotjs.2017'")
+          li(v-t="'dotjs.2018'")
       li.ri-conferences__item
         a(href="http://dotcss.io")
           img.ri-conferences__logo(src="../../assets/logos/logo-dotcss.png", alt="dotCSS logo")
-        p.ri-conferences__caption(v-t="'dotcss'")
+        ul.ri-conferences__caption
+          li(v-t="'dotcss.2017'")
+          li(v-t="'dotcss.2018'")
 </template>
 
 <script>
@@ -25,8 +29,11 @@ export default {
     &__intro
       ri-m-subtitleText($ri-tertiaryColor)
 
-    &__list
+    &__list,
+    &__caption
       ri-m-unstyleList()
+
+    &__list
       display flex
       padding 4 * $ri-baseMargin 0
 
@@ -42,12 +49,24 @@ export default {
 <i18n>
   {
     "en": {
-      "dotjs": "December 1, 2017",
-      "dotcss": "November 30, 2017"
+      "dotjs": {
+        "2017": "December 1, 2017",
+        "2018": "November 9, 2018"
+      },
+      "dotcss": {
+        "2017": "November 30, 2017",
+        "2018": "November 8, 2018"
+      }
     },
     "fr": {
-      "dotjs": "1er décembre 2017",
-      "dotcss": "30 novembre 2017"
+      "dotjs": {
+        "2017": "1er décembre 2017",
+        "2018": "9 novembre 2018"
+      },
+      "dotcss": {
+        "2017": "30 novembre 2017",
+        "2018": "8 novembre 2018"
+      }
     }
   }
 </i18n>
