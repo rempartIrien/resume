90c99328a6901cede004c4a5040940814f4f9534
Pierre Martin-Henry <pierre.martinhenry@pm.me>
2021-11-01 12:52:56 +0100

    Set dark mode according to user's preferences

--- src/common/theme.composer.ts
+++ src/common/theme.composer.ts
@@ -2,19 +2,28 @@ import { Ref, ref } from 'vue';
 
 import { Theme, darkTheme, lightTheme } from './theme.util';
 
-const isDarkMode: Ref<boolean> = ref(false);
+function setTheme(isDarkMode: Ref<boolean>): void {
+  const theme: Theme = isDarkMode.value ? darkTheme : lightTheme;
+
+  const doc: HTMLElement = document.documentElement;
+  doc.style.setProperty('--color-primary', theme.primaryColor);
+  doc.style.setProperty('--color-secondary', theme.secondaryColor);
+  doc.style.setProperty('--color-tertiary', theme.tertiaryColor);
+  doc.style.setProperty('--color-text', theme.textColor);
+  doc.style.setProperty('--color-background', theme.backgroundColor);
+}
+
+const darkThemeMq: MediaQueryList = window.matchMedia(
+  '(prefers-color-scheme: dark)'
+);
+
+const isDarkMode: Ref<boolean> = ref(darkThemeMq.matches);
+setTheme(isDarkMode);
 
 export function useTheme(): { isDarkMode: Ref<boolean>; toggle: () => void } {
   const toggle: () => void = () => {
     isDarkMode.value = !isDarkMode.value;
-    const theme: Theme = isDarkMode.value ? darkTheme : lightTheme;
-
-    const doc: HTMLElement = document.documentElement;
-    doc.style.setProperty('--color-primary', theme.primaryColor);
-    doc.style.setProperty('--color-secondary', theme.secondaryColor);
-    doc.style.setProperty('--color-tertiary', theme.tertiaryColor);
-    doc.style.setProperty('--color-text', theme.textColor);
-    doc.style.setProperty('--color-background', theme.backgroundColor);
+    setTheme(isDarkMode);
   };
 
   return { toggle, isDarkMode };
