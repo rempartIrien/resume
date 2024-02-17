963a6cae7352e2793087c70f02aa4f0f8b940dad
rempartIrien <rempart.irien@gmail.com>
2024-02-17 16:20:50 +0100

    Fixing build and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueI18n from '@intlify/vite-plugin-vue-i18n';
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueI18n({
+    VueI18nPlugin({
       compositionOnly: true,
       runtimeOnly: false,
       include: path.resolve(__dirname, './src/assets/locales/**')
