f4cf11bee78ee9862a52a78c1947b2ff151ee62c
rempartIrien <rempart.irien@gmail.com>
2024-02-23 14:12:18 +0100

    Format file

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import path from 'path';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import path from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueI18nPlugin({
-      compositionOnly: true,
-      runtimeOnly: false,
-      include: path.resolve(__dirname, './src/assets/locales/**')
-    })
-  ]
+	plugins: [
+		vue(),
+		VueI18nPlugin({
+			compositionOnly: true,
+			runtimeOnly: false,
+			include: path.resolve(__dirname, "./src/assets/locales/**"),
+		}),
+	],
 });
